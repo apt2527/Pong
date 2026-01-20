0d444a02525628fa05466d3197e3cbc6d3f02807
Arpit Abhinav Chauhan <arpit.chauhan.227@gmail.com>
2026-01-20 16:59:30 +0530

    Auto commit

---
+++ ASSETS/_readme_and_license.txt
@@ -0,0 +1,25 @@
+Bookmark Category downloaded from Freesound
+----------------------------------------
+
+"GAMES"
+
+This Bookmark Category of sounds contains sounds by the following user:
+ - Robinhood76 ( https://freesound.org/people/Robinhood76/ )
+
+
+
+
+Licenses in this Bookmark Category (see below for individual sound licenses)
+---------------------------------------------------------------
+
+Attribution NonCommercial 4.0: https://creativecommons.org/licenses/by-nc/4.0/
+
+
+Sounds in this Bookmark Category
+-------------------
+
+  * 368314__robinhood76__06901-swooshy-fight-straight-hit.wav.wav
+    * url: https://freesound.org/s/368314/
+    * license: Attribution NonCommercial 4.0
+
+
--- pong.py
+++ pong.py
@@ -1,10 +1,29 @@
 import turtle
+import time 
+import winsound
 
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
-wn.tracer(1.5)
+wn.tracer(2)
+
+# Boundary Border
+border = turtle.Turtle()
+border.speed(0)
+border.color("white")
+border.penup()
+border.goto(-390, -290)
+border.pendown()
+border.pensize(3)
+
+for i in range(2):
+    border.forward(780)   
+    border.left(90)
+    border.forward(580)   
+    border.left(90)
+
+border.hideturtle()
 
 # Score
 score_a = 0
@@ -35,8 +54,9 @@ ball.shape("circle")
 ball.color("red")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+ball.dx = -.89
+ball.dy = .78
+time.sleep(0.016)
 
 # Pen
 pen = turtle.Turtle()
@@ -44,29 +64,26 @@ pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260)
+pen.goto(0, 230)
 pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Functions
 def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 20
-    paddle_a.sety(y)
+    if paddle_a.ycor() < 240:
+        paddle_a.sety(paddle_a.ycor() + 20)
 
 def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
+    if paddle_a.ycor() > -240:
+        paddle_a.sety(paddle_a.ycor() - 20)
 
 def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 20
-    paddle_b.sety(y)
+    if paddle_b.ycor() < 240:
+        paddle_b.sety(paddle_b.ycor() + 20)
 
 def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 20
-    paddle_b.sety(y)
+    if paddle_b.ycor() > -240:
+        paddle_b.sety(paddle_b.ycor() - 20)
+
 
 # Controls
 wn.listen()
@@ -87,10 +104,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("ASSETS/WALL.wav", winsound.SND_ASYNC) 
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("ASSETS/WALL.wav", winsound.SND_ASYNC) 
         
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -110,8 +129,14 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-
+        winsound.PlaySound("/ASSETS/HIT.wav", winsound.SND_ASYNC)
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
-        ball.dx *= -1    
+        ball.dx *= -1   
+        winsound.PlaySound("/ASSETS/HIT.wav", winsound.SND_ASYNC)
 
+# AI Player
+    # if paddle_b.ycor() < ball.ycor() and abs(paddle_b.ycor() - ball.ycor() > 10):
+    #         paddle_b_up()
+    # elif paddle_b.ycor() > ball.ycor() and abs(paddle_b.ycor() - ball.ycor() > 10):
+    #         paddle_b_down()
